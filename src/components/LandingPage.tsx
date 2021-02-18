8a5bc4d937aabc5417f171400346423392caf864
Evan O'Connor <evanoc3@gmail.com>
2021-02-18 15:13:16 +0000

    Added image to homepage, Changed viewport to fit notches on > iphone X, Setup parcel@1.0.0 and autoprefixer

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,14 +1,19 @@
-import { h, FunctionComponent, RenderableProps } from "preact"; 
+import { h, FunctionComponent, RenderableProps } from "preact";// @ts-ignore
+import jhFul from "/images/jake-hills/ful.jpg"; // @ts-ignore
+import jhLrg from "/images/jake-hills/lrg.jpg"; // @ts-ignore
+import jhMed from "/images/jake-hills/med.jpg"; // @ts-ignore
+import jhSml from "/images/jake-hills/sml.jpg";
 
 
 type Props = RenderableProps<{
-}>;
+}>
 
 
 const LandingPage: FunctionComponent<Props> = () => {
 	return (
 		<>
-		<h1>Hello, World! Preact I am</h1>
+			<img id="hero" srcSet={`${jhFul} 5073w, ${jhLrg} 2400w, ${jhMed} 1920w, ${jhSml} 640w`} />
+			<div style={{height: "1000px"}}></div>
 		</>
 	);
 }
