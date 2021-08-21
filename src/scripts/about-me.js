2fd3f6bcd0cc55a31731bb70af3a58655532049d
Evan O'Connor <evanoc3@gmail.com>
2021-08-22 00:28:46 +0100

    Added an "about me" section

---
+++ src/scripts/about-me.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+const container = document.getElementById("about__wrapper");
+const img = document.getElementById("about__img");
+
+
+var imageIsVisible = false;
+
+
+function setup() {
+	img.classList.add("hidden");
+	window.addEventListener("scroll", onScrollHandler);
+	onScrollHandler();
+}
+
+
+function onScrollHandler() {
+	const coords = container.getBoundingClientRect();
+
+	if(!imageIsVisible && coords.top > 0 && coords.top < (window.innerHeight * 0.6)) {
+		imageIsVisible = true;
+		window.removeEventListener("scroll", onScrollHandler)
+		img.classList.remove("hidden");
+	}
+}
+
+
+setup();
